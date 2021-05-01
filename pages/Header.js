85adad7e91ea912383c5522802703e0807e09d51
Elias Junior <elijah.junnior@outlook.com>
2021-05-01 01:25:12 -0300

    adding react pages in te React with next project :kissing_heart::grin:

--- pages/Header.js
+++ pages/Header.js
@@ -2,7 +2,7 @@ function Header() {
     return (
         <header style={{ 'padding': '0px', }}>
             <h1 style={{ 'display': 'inline-block', 'width': '30px', textAlign: 'left' }} > Icon </h1>
-            <h4 style={{ 'display': 'inline-block', 'width': '870px', textAlign: 'center' }} > NLW 5 - Trilha de React </h4>
+            <h4 style={{ 'display': 'inline-block', 'width': '770px', textAlign: 'center' }} > NLW 5 - Trilha de React </h4>
             <hr />
         </header >
     );
