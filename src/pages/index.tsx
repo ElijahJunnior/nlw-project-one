3abc235fcf412280114f602d1f960b8fa40733d7
Elias Junior <elijah.junnior@outlook.com>
2021-05-28 09:05:02 -0300

    testing api with useEffect :heart_eyes::flushed::grin:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
 import ContaClick from './ContaClick';
+import { useEffect } from 'react';
 
 export default function Home() {
+
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+  }, []);
+
   return (
     <div>
       <div className="ContentGeneral">
+
+
         <p> Index </p>
+
         <br /> <br /> <br /> <br /> <br /> <br /> <br />
+
       </div>
     </div>
   )
-}
+} 
