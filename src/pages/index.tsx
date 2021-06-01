f29b3c5abe8e3c16f3de73ac61f5a443c9b06d0b
Elias Junior <elijah.junnior@outlook.com>
2021-06-01 06:15:42 -0300

    get content of api with Server Side Render (SSR) Method :tada:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,22 @@
 import ContaClick from './ContaClick';
 import { useEffect } from 'react';
 
-export default function Home() {
-
-  useEffect(() => {
-    fetch('http://localhost:3333/episodes')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, []);
-
+export default function Home(props) {
   return (
     <div>
       <div className="ContentGeneral">
-
-
-        <p> Index </p>
-
-        <br /> <br /> <br /> <br /> <br /> <br /> <br />
-
+        <p> {JSON.stringify(props.episodes)} </p>
       </div>
     </div>
   )
-} 
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+  return {
+    props: {
+      episodes: data,
+    }
+  }
+}
