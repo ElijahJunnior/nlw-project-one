6c200754d577713371348ec1ada944992bd0640a
Elias Junior <elijah.junnior@outlook.com>
2021-06-02 06:59:22 -0300

    Consume function changed to use TypeScript :tada::tada::tada:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,27 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next'
+
+type Episode = {
+  id: string,
+  title: string,
+  members: string,
+  published_at: string,
+  thumbnail: string,
+  description: string
+  file: {
+    url: string,
+    type: string,
+    duration: number,
+  }
+}
+
+type HomeProps = {
+  episodes: Episode[],
+}
+
+export default function Home(props: HomeProps) {
+
+  console.log(props.episodes)
+
   return (
     <div>
       <div className="ContentGeneral">
@@ -8,7 +31,7 @@ export default function Home(props) {
   )
 }
 
-export async function getStaticProps() {
+export const getstaticProps: GetStaticProps = async () => {
   const response = await fetch('http://localhost:3333/episodes');
   const data = await response.json();
   return {
