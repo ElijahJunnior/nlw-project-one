b6f0a40f4faae9c191fdd3adf0c1532c210d4529
Elias Junior <elijah.junnior@outlook.com>
2021-06-16 07:13:05 -0300

    starting Player Context code :scream::fearful::sob:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,15 +7,19 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Player from '../components/Player';
 
+import { PlayerContext } from '../contexts/PlayerContext';
+
 function MyApp({ Component, pageProps }) {
   return (
     <div className={styles.wrapper}>
-      <main >
-        <Header />
-        <Component {...pageProps} />
-        {/* <Footer /> */}
-      </main>
-      <Player />
+      <PlayerContext.Provider value={''}>
+        <main >
+          <Header />
+          <Component {...pageProps} />
+          {/* <Footer /> */}
+        </main>
+        <Player />
+      </PlayerContext.Provider>
     </div >
   )
 }
